fe157caad350e569fb7696de221a0906ac1e52ed
Paulo <pauloemidioazevedo@gmail.com>
2020-12-17 10:48:07 -0300

    minors

---
+++ BootCamp/Semana _ 1/web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+//Fragmento( <> </> ) é quando precisamos criar um container e ao mesmo tempo não criar nenhum efeito no HTML! <> </> 
+
+function App() {
+  return (
+    <Header />
+  )
+}
+
+export default App;
